dc862aaa256d0723485233d1b2cd4924715af9cf
pao-pipat <nichakorn.pipat@gmail.com>
2024-10-25 13:09:17 +0100

    test both sub and bolus cases

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -18,23 +18,21 @@ def test_solve():
                 'Dosing_Type': 'Bolus'
             }
     model2 = {
-    'name': 'Subcutaneous_Model',
-    'Q_p1': 2.0,
-    'V_c': 1.0,
-    'V_p1': 1.0,
-    'CL': 1.0,
-    'ka': 1.0,
-    'X': 1.0,
-    'Dosing_Type': 'Sub'
+                'name': 'model2',
+                'Q_p1': 2.0,
+                'V_c': 1.0,
+                'V_p1': 1.0,
+                'CL': 1.0,
+                'ka': 1.0,
+                'X': 1.0,
+                'Dosing_Type': 'Sub'
             }
     t_eval = np.linspace( 0 ,10 ,10000)
     y0 = np.array([0.0, 0, 0])
-    sol = pk.Solution(args_dict = model1, t_eval= t_eval , y0 = y0)
-    sol.define_peripheral_compartments(1)
-    sol.solve()
-    sol = pk.Solution(args_dict = model2, t_eval= t_eval , y0 = y0)
-    sol.define_peripheral_compartments(1)
-    sol.solve()
+    for model in [model1, model2]:
+        sol = pk.Solution(args_dict = model, t_eval= t_eval , y0 = y0)
+        sol.define_peripheral_compartments(1)
+        sol.solve()
     assert isfile("model1.npz") == True
     assert isfile("model2.npz") == True
 
@@ -51,24 +49,21 @@ def test_plot():
                 'Dosing_Type': 'Bolus'
             }
     model2 = {
-    'name': 'Subcutaneous_Model',
-    'Q_p1': 2.0,
-    'V_c': 1.0,
-    'V_p1': 1.0,
-    'CL': 1.0,
-    'ka': 1.0,
-    'X': 1.0,
-    'Dosing_Type': 'Sub'
+                'name': 'model2',
+                'Q_p1': 2.0,
+                'V_c': 1.0,
+                'V_p1': 1.0,
+                'CL': 1.0,
+                'ka': 1.0,
+                'X': 1.0,
+                'Dosing_Type': 'Sub'
             }   
     t_eval = np.linspace( 0 ,10 ,10000)
     y0 = np.array([0.0, 0, 0])
-    sol = pk.Solution(args_dict = model1, t_eval= t_eval , y0 = y0)
-    sol.define_peripheral_compartments(1)
-    sol.solve()
-    sol.Plot()
-    sol = pk.Solution(args_dict = model2, t_eval= t_eval , y0 = y0)
-    sol.define_peripheral_compartments(1)
-    sol.solve()
-    sol.Plot()
+    for model in [model1, model2]:
+        sol = pk.Solution(args_dict = model, t_eval= t_eval , y0 = y0)
+        sol.define_peripheral_compartments(1)
+        sol.solve()
+        sol.Plot()
     assert isfile("model1.png") == True
     assert isfile("model2.png") == True
