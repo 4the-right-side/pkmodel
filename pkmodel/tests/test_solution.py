e13ba67b38f8092373dfc66e616dd4e73417cc9e
pao-pipat <nichakorn.pipat@gmail.com>
2024-10-25 11:27:03 +0100

    add some tests to solution.py

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -1,20 +1,44 @@
 # import unittest
 # import pkmodel as pk
+import pytest
+import pkmodel as pk
+from os.path import isfile
+
+def test_solve():
+    model = pk.Protocol(args_dict = {
+                'name': 'model1',
+                'Q_p1': 1.0,
+                'V_c': 1.0,
+                'V_p1': 1.0,
+                'CL': 1.0,
+                'X': 1.0,
+                'ka': 0,
+                'Dosing_Type': 'Bolus'
+            },)
+    t_eval = np.linspace( 0 ,10 ,10000)
+    y0 = np.array([0.0, 0, 0])
+    sol = Solution(args_dict = model, t_eval= t_eval , y0 = y0)
+    sol.define_peripheral_compartments(1)
+    sol.solve()
+    assert isfile("model1.npz") == True
+
+def test_plot():
+    model = pk.Protocol(args_dict = {
+                'name': 'model1',
+                'Q_p1': 1.0,
+                'V_c': 1.0,
+                'V_p1': 1.0,
+                'CL': 1.0,
+                'X': 1.0,
+                'ka': 0,
+                'Dosing_Type': 'Bolus'
+            },)
+    t_eval = np.linspace( 0 ,10 ,10000)
+    y0 = np.array([0.0, 0, 0])
+    sol = Solution(args_dict = model, t_eval= t_eval , y0 = y0)
+    sol.define_peripheral_compartments(1)
+    sol.solve()
+    sol.Plot()
+    assert isfile("model1.png") == True
 
 
-# class SolutionTest(unittest.TestCase):
-#     """
-#     Tests the :class:`Solution` class.
-#     """
-#     def test_create(self):
-#         """
-#         Tests Solution creation.
-#         """
-#         model = pk.Solution()
-#         self.assertEqual(model.value, 44)
-
-#Need to implement tests for the following functions:
-
-#pk.Solution.solve (may include checking that RHS equations work, and that an array is generated)
-
-#pk.Solution.Plot (check the graphs are produced as pngs in the right place (?))
